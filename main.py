700790d93b66f641720afbeebe205d0ef62623fe
Lucky negi <luckynegi1025@gmail.com>
2025-08-24 19:37:24 +0530

    done

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://frontend-winning-77as.vercel.app/"],
+    allow_origins=["http://localhost:3000","https://frontend-winning-77as.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
